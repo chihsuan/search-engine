63b796ae7cfc1bd342ea5652e2b7fab149209095
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-14 23:21:26 +0800

    tokenize and stemmer

--- text_processing.py
+++ text_processing.py
@@ -3,7 +3,36 @@
 
 import sys
 import os
+import nltk
+
+from my_class.Document import Document
+from my_class.Tokenizer import Tokenizer
+from doc_preprocessing import get_docs_list
+
 
 def to_db():
+    pass
 
-if __name__=='__name__':
+if __name__=='__main__':
+    if len(sys.argv) >= 2: 
+        doc_input = sys.argv[1]
+    else:
+        doc_input = 'output/en_doc/'
+        
+    document_list = get_docs_list(doc_input)
+    tokenizer = Tokenizer()
+    for doc in document_list:
+        doc_id = 0
+        doc_obj = Document(doc_id, doc, doc_input)
+        # tokenize
+        for line in doc_obj.get_lines():
+            tokens = tokenizer.to_tokens(line.decode('utf-8'))
+            for token in tokens:
+                if tokenizer.is_stop_word(token) or token.isdigit():
+                    token = ""
+                else:
+                    token = tokenizer.stemming(token)
+                    print token.encode('utf-8')
+        del doc_obj
+        break
+        doc_id += 1
