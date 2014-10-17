a288715c91f2ec471ee4345fd5e5205f8bb49335
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-17 17:43:35 +0800

    add test and helper

--- search_engine/test/controllers/doc_lookups_controller_test.rb
+++ search_engine/test/controllers/doc_lookups_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TermsControllerTest < ActionController::TestCase
+class DocLookupsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
---
+++ search_engine/test/helpers/doc_lookups_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DocLookupsHelperTest < ActionView::TestCase
+end
