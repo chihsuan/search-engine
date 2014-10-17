705d3c44ca559737564cd5e3ddf32ed0994fa3ee
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-17 21:11:04 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ python language_processing.py
 python text_processing.py
 python term_frequency.py
 python term_weighting.py
+python zh_n_gram.py
+python term_frequency.py output/zh_tokens/
+python term_weighting.py output/zh_tf/
