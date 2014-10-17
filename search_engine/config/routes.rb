0699f5e68c5963fd06395fef2f7b9558b3877a3c
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-10-17 17:43:09 +0800

    update router path

--- search_engine/config/routes.rb
+++ search_engine/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'doc_lookups#index'
+  get '/search' => 'doc_lookups#search'
+  resources :doc_lookups
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
